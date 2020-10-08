d34494aaeb783c23234a8948f75ff7d85a05eee5
netterc <christian.Netter@otis.com>
2020-10-08 13:11:21 -0400

    Slow down while turning.

--- robot_controller.c
+++ robot_controller.c
@@ -1,21 +1,36 @@
 
 #include "robot_controller.h"
 
-t_controller_state s;
+volatile t_controller_state s;
 
+const s16 LINE_CENTER_VALUE = 2000; //the pololu read_line() API returns a value of 2000 if the robot is centered on the line
+const u16 LINE_NOISE_FLOOR = 200;
+const s16 NOMINAL_SPEED = 100;
 
 void robot_controller(void)
 {
-	s16 line;
+	s16 position;
+	s16 steering;
+	static s16 last_steering = 0;
+	s16 speed_adjustment;
 
-	line = (s16)s.in.line - 2000;
-	line = line / 20;
+	//how far off the line center are we?   position==0 means we are on center more or less
+	position = (s16)s.in.line - LINE_CENTER_VALUE;
 
-	if ((s.in.l_s_1 < 200) && (s.in.l_s_2 < 200) && (s.in.l_s_3 < 200) && (s.in.l_s_4 < 200) && (s.in.l_s_5 < 200))
+	steering = position / 20;
+
+	//if we completely lost the line, take a default action
+	if ((s.in.l_s_1 < LINE_NOISE_FLOOR) && (s.in.l_s_2 < LINE_NOISE_FLOOR) && (s.in.l_s_3 < LINE_NOISE_FLOOR) && (s.in.l_s_4 < LINE_NOISE_FLOOR) && (s.in.l_s_5 < LINE_NOISE_FLOOR))
 	{
-		//we lost the line; go straight
-		line = 0;
+		steering = 0;					//we lost the line; go straight
+		//steering = last_steering;		//keept the previous steering angle
 	}
-	s.out.lm = 100 + line;
-	s.out.rm = 100 - line;
+
+	//if we end up having to steer either left or right, then lets also slow down a little bit. The harder we are steering, the more we should slow down.
+	speed_adjustment = abs(steering) / 2;
+
+	s.out.lm = NOMINAL_SPEED + steering - speed_adjustment;
+	s.out.rm = NOMINAL_SPEED - steering - speed_adjustment;
+
+	last_steering = steering;
 }
