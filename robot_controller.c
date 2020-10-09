907ec5eef7a4010648a52babceb44859c217ee52
nettercm <chris@chrisnetter.com>
2020-10-09 12:01:40 -0400

    Make sure mixing of C and C++ is possible.

--- robot_controller.c
+++ robot_controller.c
@@ -1,11 +1,20 @@
 
 #include "robot_controller.h"
 
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+
 volatile t_controller_state s;
 
 const s16 LINE_CENTER_VALUE = 2000; //the pololu read_line() API returns a value of 2000 if the robot is centered on the line
 const u16 LINE_NOISE_FLOOR = 200;
-const s16 NOMINAL_SPEED = 100;
+const s16 NOMINAL_SPEED = 50;
 
 void robot_controller(void)
 {
@@ -33,4 +42,22 @@ void robot_controller(void)
 	s.out.rm = NOMINAL_SPEED - steering - speed_adjustment;
 
 	last_steering = steering;
+
+#ifdef WIN32
+
+	printf("%6lu:  l=%4d,%4d,%4d,%4d,%4d,  L=%4d,  s=%5d,  sa=%5d  m=%4d,%4d\n",
+		s.elapsed_milliseconds,
+		s.in.l_s_1, s.in.l_s_2, s.in.l_s_3, s.in.l_s_4, s.in.l_s_5, 
+		s.in.line,
+		steering, speed_adjustment, 
+		s.out.lm, s.out.rm);
+
+#endif
 }
+
+
+
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
--- robot_controller.h
+++ robot_controller.h
@@ -1,37 +1,52 @@
 #ifndef _robot_controller_h_
 #define _robot_controller_h_
 
-#include "typedefs.h"
-
-
-typedef struct
+#ifdef __cplusplus
+extern "C"
 {
-	u16 l_s_1;
-	u16 l_s_2;
-	u16 l_s_3;
-	u16 l_s_4;
-	u16 l_s_5;
-	u16 line;
+#endif
+
+
+#include "typedefs.h"
+#include <stdio.h>
+#include <math.h>
+
+typedef struct t_inputs_
+{
+	u16 l_s_1,  l_s_2,  l_s_3,  l_s_4,  l_s_5;		//the 5 individual line sensors on the 3pi
+	u16 line;										//the Polou QTR read_line() API returns a composite value 
 } t_inputs;
 
 
-typedef struct
+
+typedef struct t_outputs_
 {
-	s16 lm;
-	s16 rm;
+	s16 lm, rm;		//left and rigth motor speed - in units as per Pololu set_motors() library function;
 } t_outputs;
 
 
-typedef struct
+
+typedef struct t_controller_state_
 {
 	t_inputs in;
 	t_outputs out;
+	u32 elapsed_milliseconds;
 } t_controller_state;
 
 
 extern volatile t_controller_state s;
 
 extern void robot_controller(void);
+
+
+
+
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+
 
 #endif
 
